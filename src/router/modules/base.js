ca09aa651775d3350b66c68f81b58074d234e3ff
shufei021 <1017981699@qq.com>
2021-11-12 23:11:17 +0800

    feat: edit home page

--- src/router/modules/base.js
+++ src/router/modules/base.js
@@ -2,7 +2,7 @@
  * @Description: 基础路由
  * @Author: shufei
  * @Date: 2021-11-11 20:53:50
- * @LastEditTime: 2021-11-12 12:35:17
+ * @LastEditTime: 2021-11-12 23:10:25
  * @LastEditors: shufei
  */
 export default [
@@ -12,7 +12,7 @@ export default [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/Home.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '@/views/home.vue'),
     meta: { title: '主页', keepAlive: false, isTabBar: false }
   }
 ]
