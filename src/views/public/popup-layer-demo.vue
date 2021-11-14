54781301da65f68ee3cf98bc0724c82b0a6bf995
shufei021 <1017981699@qq.com>
2021-11-14 20:08:20 +0800

    feat: update vue-popup-layer version

--- src/views/public/popup-layer-demo.vue
+++ src/views/public/popup-layer-demo.vue
@@ -2,33 +2,35 @@
  * @Description: popuplayer-demo 无限弹出层示例
  * @Author: shufei
  * @Date: 2021-11-08 16:55:32
- * @LastEditTime: 2021-11-11 19:29:53
+ * @LastEditTime: 2021-11-14 20:05:15
  * @LastEditors: shufei
 -->
 <template>
   <div class="container">
-    <h1>主页</h1>
+    <van-divider>页面首页</van-divider>
     <van-button type="primary" @click="layer.show1=true">打开第一个弹层</van-button>
 
     <!-- 第一个弹层 -->
-    <popup-layer :visible.sync="layer.show1" @onOpen="open" @onClose="close">
-      <h1>第一个弹层</h1>
+    <popup-layer :visible.sync="layer.show1" @onOpen="open" @onClose="close" :extra="{lv:1}" ref="lv1">
+      <van-divider>第一个弹层页面</van-divider>
       <van-button type="primary" @click="layer.show1=false">关闭第一个弹层（也可以点击物理返回键）</van-button>
       <van-button type="info" @click="layer.show2=true">打开第二个弹层</van-button>
-      <van-cell is-link to="/">销毁测试</van-cell>
+      <!-- <van-cell is-link > 不要再popuplayer里面销毁测试</van-cell> -->
     </popup-layer>
 
     <!-- 第二个弹层 -->
-    <popup-layer :visible.sync="layer.show2">
-      <h1>第二个弹层</h1>
+    <popup-layer :visible.sync="layer.show2" :extra="{lv:2}" @onOpen="open" @onClose="close" ref="lv2">
+      <van-divider>第二个弹层页面</van-divider>
       <van-button type="primary" @click="layer.show2=false">关闭第二个弹层（也可以点击物理返回键）</van-button>
       <van-button type="info" @click="layer.show3=true">打开第三个弹层</van-button>
+      <van-button type="warning" @click="closeAll(2)">关闭所有弹层</van-button>
     </popup-layer>
 
     <!-- 第三个弹层 -->
-    <popup-layer :visible.sync="layer.show3">
-      <h1>第三个弹层</h1>
+    <popup-layer :visible.sync="layer.show3" :extra="{lv:3}" @onOpen="open" @onClose="close" ref="lv3">
+      <van-divider>第三个弹层页面</van-divider>
       <van-button type="primary" @click="layer.show3=false">关闭第二个弹层（也可以点击物理返回键）</van-button>
+      <van-button type="warning" @click="closeAll(3)">关闭所有弹层</van-button>
     </popup-layer>
   </div>
 </template>
@@ -46,14 +48,19 @@ export default {
     }
   },
   methods: {
-    open () {
-      console.log('第一层弹层打开了')
+    open (id, extra) {
+      console.log('第' + extra.lv + '层弹层打开了', id, extra)
     },
     /**
      * @param {Boolean} isPopstate：关闭弹层事件是否来源物理返回键
      */
-    close ({ isPopstate }) {
-      console.log(isPopstate ? '点击物理返回键 把 第一层弹层关闭了' : '用其它非物理返回键方式 把 第一层弹层关闭了')
+    close ({ isPopstate }, extra) {
+      console.log(isPopstate ? '点击物理返回键 把 第' + extra.lv + '层弹层关闭了' : '用其它非物理返回键方式 把 第' + extra.lv + '层弹层关闭了', extra)
+    },
+    closeAll (lv) {
+      for (let i = 1; i <= lv; i++) {
+        this.$refs['lv' + i].closeLv()
+      }
     }
   }
 }
@@ -62,5 +69,8 @@ export default {
   .container{
     height: 100%;
     overflow: hidden;
+    button{
+      width: 100%;
+    }
   }
 </style>
